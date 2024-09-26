41be4a45effae025748074147646154a91deb143
mirafuentesjenalyn <mirafuentesjenalyn@gmail.com>
2024-09-26 08:12:28 +0800

    "Menu, Report, Products, Employees Button"

--- src/CoffeShop/Dashboard.java
+++ src/CoffeShop/Dashboard.java
@@ -261,7 +261,7 @@ public class Dashboard extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
--- src/CoffeShop/Products.java
+++ src/CoffeShop/Products.java
@@ -441,7 +441,9 @@ public class Products extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        Dashboard callDashboard = new Dashboard();
+        callDashboard.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
